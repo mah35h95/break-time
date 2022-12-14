0923431182294916e3abbc876d573fe27acf5d7d
Mahesh G <mahesh.g@tyson.com>
2022-12-14 18:42:12 +0530

    formatting

--- app.go
+++ app.go
@@ -10,42 +10,46 @@ import (
 
 func main() {
 
-	fmt.Println("Fetching Jobs from env...")
+	fmt.Println("Fetching env variables...")
+
 	jobs, ok := os.LookupEnv("JOBS")
 	if !ok || len(jobs) == 0 {
 		fmt.Println("JOBS env variable is not set in launch.json")
 		os.Exit(0)
 	}
 
+	metaSvcUrl, ok := os.LookupEnv("META_SVC_URL")
+	if !ok || len(metaSvcUrl) == 0 {
+		fmt.Println("META_SVC_URL env variable is not set in launch.json")
+		os.Exit(0)
+	}
+
+	token, ok := os.LookupEnv("TOKEN")
+	if !ok || len(token) == 0 {
+		fmt.Println("TOKEN env variable is not set in launch.json")
+		os.Exit(0)
+	}
+
+	bearer := "Bearer " + token
+
 	// jobids := []string{"", "", ""}
 	jobids := strings.Split(jobs, "/")
 
 	for i := 0; i < len(jobids); i++ {
-		fmt.Printf("%s\n", jobids[i])
-
+		fmt.Printf("%s - Loop Start\n", jobids[i])
+		err := PauseJob(jobids[i], metaSvcUrl, bearer)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("%s - Loop Complete\n", jobids[i])
 	}
-
-	// for i := 0; i < len(jobids); i++ {
-	// 	fmt.Printf("%s - Loop Start\n", jobids[i])
-	// 	err := PauseJob(jobids[i])
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 	}
-	// 	fmt.Printf("%s - Loop Complete\n", jobids[i])
-	// }
 }
 
-func PauseJob(dataSourceId string) error {
+func PauseJob(dataSourceId string, metaSvcUrl string, bearer string) error {
 	parts := strings.Split(dataSourceId, ".")
 	if len(parts) != 5 {
 		return errors.New("invalid dataSourceId " + dataSourceId)
 	}
-
-	metaSvcUrl := os.Getenv("META_SVC_URL")
-
-	token := os.Getenv("TOKEN")
-
-	bearer := "Bearer " + token
 
 	path := fmt.Sprintf("%v/callback/sources/%v/technologies/%v/databases/%v/jobs/%v.%v/pause", metaSvcUrl, parts[0], parts[1], parts[2], parts[3], parts[4])
 
@@ -69,6 +73,7 @@ func PauseJob(dataSourceId string) error {
 	}
 	defer response.Body.Close()
 
-	fmt.Printf("Job %s has been triggered to be paused", dataSourceId)
+	fmt.Printf("Job %s has been triggered to be paused\n", dataSourceId)
+
 	return nil
 }
