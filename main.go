0fa2b172a48e1f3546d12e447880a6a1dcca5ead
Mahesh G <47917543+mah35h95@users.noreply.github.com>
2024-11-08 09:26:52 +0530

    cron divide ➗🦪

--- main.go
+++ main.go
@@ -13,6 +13,13 @@ import (
 	"github.com/mah35h95/break-time/utils"
 )
 
+// CronRange - Has a min max value for a cron string
+type CronRange struct {
+	Min  int
+	Max  int
+	Cron string
+}
+
 // main - everything started here
 func main() {
 	fmt.Println("Fetching ENV variables...")
@@ -98,7 +105,7 @@ func main() {
 					err = dice.DeleteJob(dataSourceId, metaSvcUrl, bearer)
 
 				case dice.EditCron:
-					cron := "0 0 * * *"
+					cron := getCron((chunkSize * i) + j + 1)
 					err = dice.EditCronSchedule(dataSourceId, metaSvcUrl, bearer, cron)
 
 				case dice.DeleteHydratedRes:
@@ -149,4 +156,28 @@ func ValidateAndRefreshToken(metaSvcUrl, bearer string) (string, error) {
 
 	fmt.Printf("Failed to update Identity Token for %d times\nExiting...\n", retryCount)
 	return newBearer, fmt.Errorf("unable to refresh identity token")
+}
+
+// getCron - returns an increasing cron string
+func getCron(value int) string {
+	cronRanges := []CronRange{
+		{Min: 1, Max: 100, Cron: "0 0 * * *"},
+		{Min: 101, Max: 200, Cron: "0 1 * * *"},
+		{Min: 201, Max: 300, Cron: "0 2 * * *"},
+		{Min: 301, Max: 400, Cron: "0 3 * * *"},
+		{Min: 401, Max: 500, Cron: "0 4 * * *"},
+		{Min: 501, Max: 600, Cron: "0 5 * * *"},
+		{Min: 601, Max: 700, Cron: "0 6 * * *"},
+		{Min: 701, Max: 800, Cron: "0 7 * * *"},
+		{Min: 801, Max: 900, Cron: "0 8 * * *"},
+		{Min: 901, Max: 1000, Cron: "0 9 * * *"},
+	}
+
+	for _, cronRange := range cronRanges {
+		if value >= cronRange.Min && value <= cronRange.Max {
+			return cronRange.Cron
+		}
+	}
+
+	return "0 0 * * *"
 }
